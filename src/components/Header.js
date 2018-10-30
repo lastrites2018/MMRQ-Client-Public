b25b87c75ad52d849c0366936f1c84088dbcd70a
Yongseok <leeyongseok@iyongseog-ui-MacBook-Pro.local>
2018-10-30 20:15:00 +0900

    header finish

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,21 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
-      <NavLink to="/main/MMresQ" className="item" activeClassName="active">메인</NavLink>
-      <NavLink to="/posts" className="item" activeClassName="active">포스트</NavLink>
-      <NavLink to="/login" className="item" activeClassName="active">로그인</NavLink>
-      <NavLink to="/mypage" className="item" activeClassName="active">마이페이지</NavLink>
-      <NavLink to="/search" className="item" activeClassName="active">검색</NavLink>
-      <NavLink to="/signup" className="item" activeClassName="active">회원가입</NavLink>
+    <div className="header_line">
+      <div className="header">
+        <NavLink to="/signup" className="item" activeClassName="active">신고/제보 등록하기</NavLink>
+        <NavLink to="/main" className="item" activeClassName="active">HOME</NavLink>
+        <NavLink to="/posts" className="item" activeClassName="active">찾아주세요</NavLink>
+        <NavLink to="/search" className="item" activeClassName="active">신고할래요</NavLink>
+        <NavLink to="/mypage" className="item" activeClassName="active">마이페이지</NavLink>
+        <NavLink to="/login" className="item" activeClassName="active">로그인</NavLink>
+        {/* <NavLink to="/main" className="item" activeClassName="active">메인</NavLink>
+        <NavLink to="/posts" className="item" activeClassName="active">포스트</NavLink>
+        <NavLink to="/login" className="item" activeClassName="active">로그인</NavLink>
+        <NavLink to="/mypage" className="item" activeClassName="active">마이페이지</NavLink>
+        <NavLink to="/search" className="item" activeClassName="active">검색</NavLink>
+      <NavLink to="/signup" className="item" activeClassName="active">회원가입</NavLink> */}
+      </div>
     </div>
   );
 };
---
+++ src/routes/Main/main_post.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class main_post extends Component {
+  render() {
+    return (
+        <div className="main_post">
+          Main
+        </div>
+    )
+  }
+}

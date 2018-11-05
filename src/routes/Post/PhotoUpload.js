83b984da0732ae25e6fcdb67502a5e1009e27bbe
koenzi132 <daniel.k9159@gmail.com>
2018-11-05 17:02:53 +0900

    adding PhotoUpload Post page

--- src/routes/Post/PhotoUpload.js
+++ src/routes/Post/PhotoUpload.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
+import base64Img from "base64-img";
 
 class PhotoUpload extends Component {
-  fileSelectHandler = event => {
-    console.log(event.target.files[0]);
+  state = {
+    selectedFile: null,
+    base64Img: null
   };
+
+  fileSelectHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
+  fileUploadHandler = () => {};
   render() {
     return (
       <div>
         <input type="file" onChange={this.fileSelectHandler} />
+        <button onClick={this.fileUploadHandler}>Upload</button>
       </div>
     );
   }
